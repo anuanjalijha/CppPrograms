ffe114a90c19a0ea756ccf66266724f41e5c3be8
Anjali <anjalijanar@gmail.com>
2023-02-03 12:27:04 +0530

    finding the nth fibonacci no using recursion

---
+++ UsingRecursionFindFibonacciNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int solve(int n){
+	if(n == 1)
+	return 0;
+	if(n == 2)
+	return 1;
+	else
+	return solve(n-1) + solve(n-2);
+}
+int main(){
+	int n;
+	cin>>n;
+	cout<<solve(n);
+	return 0;
+}
+
