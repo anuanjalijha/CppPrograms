1e4e518ed62d79ee3a26ad4bcc45bff256e0f823
Anjali <anjalijanar@gmail.com>
2023-03-02 11:11:24 +0530

    finding the minimum number of a tank refills in a car by covering the total distance

--- MinimumNumberOfATankRefillsInACar.cpp
+++ MinimumNumberOfATankRefillsInACar.cpp
@@ -1,32 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	int d;
-	cout<<"Total Distance Covered by a car: ";
-	cin>>d;
-	int distance_bw_two_petrol_pump = 1;
-	int no_of_petrol_pump = d/distance_bw_two_petrol_pump;
-	int capacity_of_car;
-	cout<<"Max diatance travelled with tank full: ";
-	cin>>capacity_of_car;
-	int min_no;
-	
-//	if(capacity_of_car>distance_bw_two_petrol_pump){
-//		return true;
-//	}
-//	else{
-//		cout<<"Can not determined";
-//		return false;
-//		
-//	}
-	
-	if(d%capacity_of_car == 0){
-		min_no = (d/capacity_of_car)-1;
-	}
-	else{
-	min_no = d/capacity_of_car;
-	
-	}
-	cout<<min_no<<endl;
-	
+
+int main() {
+    int d ,m;
+     cout<<"enter the distance" ;
+     cin>>d;
+     cout<<"enter the capicity of a car";
+     cin>>m;
+    int arr[7] = {1, 3, 6,7, 12, 15,18};
+    int refill = 0;
+    int current = 0;
+
+
+    while (current + m < d) {
+        int last = current;
+        for (int i=0; i < 7 && arr[i] - last <= m; i++) {
+            current = arr[i]; 
+        }
+        if (current == last) {
+            cout << "Impossible to reach the destination" << endl;
+            return 0;
+        }
+        refill++;
+    }
+
+    cout << "Minimum number of refills needed: " << refill << endl;
+
+    return 0;
 }
+
